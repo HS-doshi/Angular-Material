95d4c490d08aa63bb47d70e73fc6f8b4da35d3d0
Heet Doshi <doshiheet2872003@gmail.com>
2024-02-14 13:16:13 +0530

    material concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatButtonModule } from '@angular/material/button'
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatIconModule} from '@angular/material/icon'
+import {MatBadgeModule} from '@angular/material/badge'
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatMenuModule} from '@angular/material/menu'
+import {MatListModule} from '@angular/material/list'
+import { MatDividerModule } from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list'
+import {MatExpansionModule} from '@angular/material/expansion'
+import {MatCardModule} from '@angular/material/card'
+import {MatTabsModule} from '@angular/material/tabs'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet ,
+    CommonModule ,
+    MatToolbarModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatListModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatExpansionModule,
+    MatCardModule,
+    MatTabsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Material';
+
+  notification  = 1
+  showSpinner = false
+
+  loadData(){
+    this.showSpinner = true;
+    setTimeout(()=>{
+      this.showSpinner = false;
+    },2000)
+  }
+  logChange(index : number| null){
+      console.log(index)
+  }
+
 }
